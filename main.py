43808acf761efb55e1bc8ec2376884529053bdc0
GF <gunhoflash@gmail.com>
2019-12-09 19:13:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,8 @@ from Utils import *
 WIDTH = 320
 HEIGHT = 240
 TOLERANCE = 145
-TURN_MAX = 88
-TURN_MID = 48
-
-'''
-WIDTH = 640
-HEIGHT = 480
-TOLERANCE = 300
-TURN_MAX = 168
-TURN_MID = 76
-'''
+TURN_MAX = 190
+TURN_MID = 90
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 direction = 0
@@ -38,7 +30,7 @@ if ser is None:
     ser = serial.Serial('/dev/ttyUSB1',9600)
 
 print('start')
-time.sleep(5)
+time.sleep(3)
 
 for q in range(N_SLICES):
     Images.append(Image())
@@ -51,6 +43,9 @@ def in_tolerance(n):
     return True
 
 def get_cmd(y1, y2, y3, y4, y5, y6):
+    
+    num_valid = 6
+    
     y1 -= WIDTH/2
     y2 -= WIDTH/2
     y3 -= WIDTH/2
@@ -58,33 +53,51 @@ def get_cmd(y1, y2, y3, y4, y5, y6):
     y5 -= WIDTH/2
     y6 -= WIDTH/2
     
+    master_point = 0
+    
     # +: right
     # -: left
-    master_point = (y1 + y2 + y3 + y4 + y5 + y6) * 0.25
+    if in_tolerance(y1) == False:
+        num_valid -= 1
+        y1 = 0
+    if in_tolerance(y2) == False:
+        num_valid -= 1
+        y2 = 0
+    if in_tolerance(y3) == False:
+        num_valid -= 1
+        y3 = 0
+    if in_tolerance(y4) == False:
+        num_valid -= 1
+        y4 = 0
+    if in_tolerance(y5) == False:
+        num_valid -= 1
+        y5 = 0
+    if in_tolerance(y6) == False:
+        num_valid -= 1
+        y6 = 0
     
-    if in_tolerance(y1):
-        master_point += y1 * 0.85
-    if in_tolerance(y2):
-        master_point += y2 * 0.25
-    if in_tolerance(y3):
-        master_point += y3 * 0.23
+    master_point = 2.65 * (y1 * 0.7 + y2 * 0.85 + y3 + y4 * 1.1 + y5 * 1.2 + y6 * 1.35) / (num_valid + 0.1)
 
-    if in_tolerance(y4):
-        master_point -= y4 * 0.27
-    if in_tolerance(y5):
-        master_point -= y5 * 0.75
-    if in_tolerance(y5):
-        master_point -= y6 * 1.15
- 
-    direction = 'G'
-    if master_point > TURN_MID and master_point < TURN_MAX :
-        direction = 'l'
-    if master_point < -TURN_MID and master_point > -TURN_MAX :
-        direction = 'r'
-    if master_point >= TURN_MAX :
-        direction = 'L'
-    if master_point <= -TURN_MAX :
-        direction = 'R'
+    master_point += y1 * 0.5
+    master_point += y2 * 0.4
+    master_point += y3 * 0.3
+    master_point -= y4 * 0.4
+    master_point -= y5 * 0.5
+    master_point -= y6 * 0.6
+
+    # back
+    if num_valid < 2:
+        direction = 'B'
+    else:
+        direction = 'G'
+        if master_point > TURN_MID and master_point < TURN_MAX :
+            direction = 'l'
+        if master_point < -TURN_MID and master_point > -TURN_MAX :
+            direction = 'r'
+        if master_point >= TURN_MAX :
+            direction = 'L'
+        if master_point <= -TURN_MAX :
+            direction = 'R'
 
     cmd = ("%c\n" % (direction)).encode('ascii')
 
@@ -92,17 +105,15 @@ def get_cmd(y1, y2, y3, y4, y5, y6):
     
     ser.write(cmd)
     print("send")
-    #   For debugging, read cmd from arduino and print it    
+    # read cmd from arduino and print it    
     read_serial = ser.readline()
     print("<<< %s" % (read_serial))
-    #direction = 'S'
-    #ser.write("S0\n".encode('ascii'));
 
 img = cv2.VideoCapture(0)
 img.set(cv2.CAP_PROP_FPS, 30)
 img.set(cv2.CAP_PROP_SATURATION, 0)
-img.set(cv2.CAP_PROP_BRIGHTNESS, 0.6)
-#img.set(cv2.CAP_PROP_CONTRAST, 0.6)
+img.set(cv2.CAP_PROP_BRIGHTNESS, 0.61)
+img.set(cv2.CAP_PROP_CONTRAST, 0.54)
 img.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 img.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 
@@ -116,10 +127,6 @@ while True:
         #이미지를 조각내서 윤곽선을 표시하게 무게중심 점을 얻는다
         Points = SlicePart(fram, Images, N_SLICES)
         print('Points : ', Points)
-
-        #N_SLICES 개의 무게중심 점을 x좌표, y좌표끼리 나눈다
-        #x = Points[::2]
-        #y = Points[1::2]
            
         #조각난 이미지를 한 개로 합친다
         fm = RepackImages(Images)
@@ -129,8 +136,8 @@ while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
         
+        # command
         get_cmd(Points[0][0], Points[1][0], Points[2][0], Points[3][0], Points[4][0], Points[5][0])
 
     else:
         print('not even processed')
-
