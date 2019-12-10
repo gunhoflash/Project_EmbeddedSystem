b9b0f271c6ffaf68d382ada88c9ed0f0f14c0790
GF <gunhoflash@gmail.com>
2019-12-10 12:28:33 +0900

    Speed Up

--- Serial.ino
+++ Serial.ino
@@ -1,5 +1,5 @@
 #include <AFMotor.h>
-#define R_MAX 215
+#define R_MAX 230
 #define L_MAX 255
 
 // Motor L293D 3:M3, 4:M4
@@ -7,13 +7,13 @@ AF_DCMotor motor_L(3);
 AF_DCMotor motor_R(4);
 
 void cmd() {
-  byte DataToRead[3];
-  DataToRead[2] = '\n';
-  Serial.readBytesUntil(char(13), DataToRead, 3);
+  byte DataToRead[2];
+  DataToRead[1] = '\n';
+  Serial.readBytesUntil(char(13), DataToRead, 2);
 
   char direction_type = DataToRead[0]; // L R l r G B
   int i = 1;
-  while (DataToRead[i] != '\n' && i < 3) i++;
+  while (DataToRead[i] != '\n' && i < 2) i++;
 
   switch (direction_type) {
     case 'L':
@@ -21,46 +21,48 @@ void cmd() {
       motor_R.setSpeed(R_MAX);
       motor_L.run(RELEASE);
       motor_R.run(FORWARD);
-      delay(200);
+      delay(210);
       break;
     case 'R':
       motor_L.setSpeed(L_MAX);
       motor_R.setSpeed(R_MAX);
       motor_L.run(FORWARD);
       motor_R.run(RELEASE);
-      delay(200);
+      delay(210);
       break;
     case 'l':
       motor_L.setSpeed(L_MAX);
       motor_R.setSpeed(R_MAX);
       motor_L.run(RELEASE);
       motor_R.run(FORWARD);
-      delay(120);
+      delay(130);
       break;
     case 'r':
       motor_L.setSpeed(L_MAX);
       motor_R.setSpeed(R_MAX);
       motor_L.run(FORWARD);
       motor_R.run(RELEASE);
-      delay(120);
+      delay(130);
       break;
     case 'G':
       motor_L.setSpeed(L_MAX);
       motor_R.setSpeed(R_MAX);
       motor_L.run(FORWARD);
       motor_R.run(FORWARD);
-      delay(300);
+      delay(400);
       break;
     case 'B':
       motor_L.setSpeed(L_MAX);
       motor_R.setSpeed(R_MAX);
       motor_L.run(BACKWARD);
       motor_R.run(BACKWARD);
-      delay(250);
+      delay(300);
+      break;
     default:
       motor_L.run(RELEASE);
       motor_R.run(RELEASE);
-      break;
+      Serial.write("ACK\n");
+      return;
   }
   
   // For Debugging, send string to RPi
